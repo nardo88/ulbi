3dfd312605b291b1fff3953e3e5b73fbbcf9fb3f
nardo88 <nardo1988@mail.ru>
2022-10-19 22:10:42 +0300

    Создал переменные для CSS и создал тему

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import { Suspense } from "react";
+import { Suspense, useContext, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import "./styles/index.scss";
 import { AboutPageAsync } from "./components/pages/AboutPage/AboutPage.async";
 import { Link } from "react-router-dom";
 import { MainPageAsync } from "./components/pages/MainPage/MainPage.async";
+import { Theme, ThemeContext } from "./theme/ThemeContext";
+import { useTheme } from "./theme/useTheme";
 
 function App() {
+  const { theme, toggleTheme } = useTheme();
   return (
-    <div className="main">
+    <div className={`main ${theme}`}>
       <Link to={"/"}>Главная</Link>
       <Link to={"/about"}>О нас</Link>
+      <button onClick={toggleTheme}>Toggle</button>
 
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
