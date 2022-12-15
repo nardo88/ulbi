2aad0327e9bd652455f1ef3c561cbfbe9827a5a1
nardo88 <nardo1988@mail.ru>
2022-12-15 21:40:34 +0300

    profile slice

--- src/pages/ProfilePage/ui/ProfilePage.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.tsx
@@ -1,15 +1,28 @@
 import { FC } from 'react'
 import { classNames } from 'helpers/classNames/classNames'
 import { useTranslation } from 'react-i18next'
+import {
+  DinamicModuleLoader,
+  ReducerList,
+} from 'shared/lib/components/DinamicModuleLoader/DinamicModuleLoader'
+import { profileReducer } from 'entities/Profile'
 
 interface ProfilePage {
   className?: string
 }
 
+const redusers: ReducerList = {
+  profile: profileReducer,
+}
+
 const ProfilePage: FC<ProfilePage> = ({ className }) => {
   const { t } = useTranslation()
 
-  return <div className={classNames('', {}, [className])}>{t('Профиль')}</div>
+  return (
+    <DinamicModuleLoader reducers={redusers} removeAfterUnmount>
+      <div className={classNames('', {}, [className])}>{t('Профиль')}</div>
+    </DinamicModuleLoader>
+  )
 }
 
 export default ProfilePage
